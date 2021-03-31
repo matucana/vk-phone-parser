8cf189e878b895a26ab21aa1f6423a89512ba122
Dialog Box Studio <79014508+matucana@users.noreply.github.com>
2021-03-31 23:34:50 +0300

    Update App.php

--- src/App.php
+++ src/App.php
@@ -33,23 +33,31 @@ class App
             $result = $this->vkApiClient->users()->get($_ENV['VK_SERVICE_KEY'], $params);
             $data = $this->validator($this->parser($result));
             $json = json_encode($data);
-            file_put_contents(__DIR__.'/../json/'.($this->usersUidProvider->getStart()+999).'.json',$json);
+            file_put_contents(__DIR__ . '/../json/' . ($this->usersUidProvider->getStart() + 999) . '.json', $json);
 
             if ($all_search === 0) {
-                yield 'Запускаем сканирование'.PHP_EOL.'Сканируем '.$this->usersUidProvider->getLimit().' пользователей'.PHP_EOL;
+                yield 'Запускаем сканирование' . PHP_EOL . 'Сканируем ' . $this->usersUidProvider->getLimit(
+                    ) . ' пользователей' . PHP_EOL;
             }
             $all_search = $all_search + count($data);
-            yield 'Обработано '.($this->usersUidProvider->getStart()+999).' => Найдено номеров: '.count($data).' | Всего найдено: '.$all_search.' | Осталось обработать '.($this->usersUidProvider->getLimit()-($this->usersUidProvider->getStart()+999));
+            yield 'Обработано ' . ($this->usersUidProvider->getStart() + 999) . ' => Найдено номеров: ' . count(
+                    $data
+                ) . ' | Всего найдено: ' . $all_search . ' | Осталось обработать ' . ($this->usersUidProvider->getLimit(
+                    ) - ($this->usersUidProvider->getStart() + 999));
             //sleep(2);
         }
-        yield 'Сканирование завершено!'.PHP_EOL.'Обработано: '.$this->usersUidProvider->getLimit().' пользователей'.PHP_EOL.'Найдено номеров: '.$all_search;
+        yield 'Сканирование завершено!' . PHP_EOL . 'Обработано: ' . $this->usersUidProvider->getLimit(
+            ) . ' пользователей' . PHP_EOL . 'Найдено номеров: ' . $all_search;
     }
 
     public function parser(array $result): array
     {
-        return array_filter($result, function($v) {
-            return isset($v["mobile_phone"]);
-        });
+        return array_filter(
+            $result,
+            function ($v) {
+                return isset($v["mobile_phone"]);
+            }
+        );
     }
 
     public function phoneValidator(string $phone): bool
@@ -64,9 +72,12 @@ class App
 
     public function validator(array $result): array
     {
-        return array_filter($result, function ($v) {
-            return $this->phoneValidator($v["mobile_phone"]) === true;
-        });
+        return array_filter(
+            $result,
+            function ($v) {
+                return $this->phoneValidator($v["mobile_phone"]) === true;
+            }
+        );
     }
 
 
