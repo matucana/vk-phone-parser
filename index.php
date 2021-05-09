63e086ecbc24a1f3d01426199a8d4153a241ea4c
Dialog Box Studio <79014508+matucana@users.noreply.github.com>
2021-05-09 18:06:30 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,21 +1,19 @@
 <?php
 
-require_once __DIR__.'/vendor/autoload.php';
+require_once __DIR__ . '/vendor/autoload.php';
+
 use Symfony\Component\Dotenv\Dotenv;
 use VK\Client\VKApiClient;
 
 
 $dotenv = new Dotenv();
-$dotenv->load(__DIR__.'/.env');
+$dotenv->load(__DIR__ . '/.env');
 
 
-$vk = new VKApiClient('5.130',  VK\Client\Enums\VKLanguage::RUSSIAN);
-
-
-
+$vk = new VKApiClient('5.130', VK\Client\Enums\VKLanguage::RUSSIAN);
 $usersUidProvider = new \Matucana\VkPhoneParser\UsersUidProvider();
 
 $app = new \Matucana\VkPhoneParser\App($vk, $usersUidProvider);
 foreach ($app->run() as $value) {
-    echo $value."\n";
+    echo $value . "\n";
 }
