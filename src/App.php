5f6a1f76da8cd1afa1852f564b79293319e186a8
Dialog Box Studio <79014508+matucana@users.noreply.github.com>
2021-05-09 18:06:22 +0300

    Update App.php

--- src/App.php
+++ src/App.php
@@ -4,8 +4,6 @@
 namespace Matucana\VkPhoneParser;
 
 
-use Brick\PhoneNumber\PhoneNumber;
-use Brick\PhoneNumber\PhoneNumberParseException;
 use Generator;
 use VK\Client\VKApiClient;
 use League\Csv\Writer;
@@ -25,8 +23,9 @@ class App
     public function run(): Generator
     {
         $writer = Writer::createFromPath(__DIR__ . '/../file.csv', 'w+');
-
+        $writer->insertOne(['ID Вконтакте', 'Имя', 'Фамилия', 'Номер телефона']);
         $all_search = 0;
+
         foreach ($this->usersUidProvider->getSeriesUid() as $user_ids) {
             $params = [
                 'user_ids' => $user_ids,
@@ -35,8 +34,7 @@ class App
 
             $result = $this->vkApiClient->users()->get($_ENV['VK_SERVICE_KEY'], $params);
             $data = $this->validator($this->parser($result));
-
-            $writer->insertAll($data);
+            $writer->insertAll($this->clearArray($data));
 
             if ($all_search === 0) {
                 yield 'Запускаем сканирование' . PHP_EOL . 'Сканируем ' . $this->usersUidProvider->getLimit(
@@ -49,7 +47,6 @@ class App
                     $data
                 ) . ' | Всего найдено: ' . $all_search . ' | Осталось обработать ' . ($this->usersUidProvider->getLimit(
                     ) - ($this->usersUidProvider->getStart() + 999));
-            //sleep(2);
         }
         yield 'Сканирование завершено!' . PHP_EOL . 'Обработано: ' . $this->usersUidProvider->getLimit(
             ) . ' пользователей' . PHP_EOL . 'Найдено номеров: ' . $all_search;
@@ -67,10 +64,14 @@ class App
 
     public function phoneValidator(string $phone): bool
     {
+        $phoneUtil = \libphonenumber\PhoneNumberUtil::getInstance();
         try {
-            PhoneNumber::parse($phone, 'RU');
-            return true;
-        } catch (PhoneNumberParseException $e) {
+            $swissNumberProto = $phoneUtil->parse($phone, "RU");
+            if ($phoneUtil->isValidNumber($swissNumberProto)) {
+                return true;
+            }
+            return false;
+        } catch (\libphonenumber\NumberParseException $e) {
             return false;
         }
     }
@@ -80,9 +81,23 @@ class App
         return array_filter(
             $result,
             function ($v) {
-                return $this->phoneValidator($v["mobile_phone"]) === true;
+                return ($this->phoneValidator($v["mobile_phone"]) === true);
             }
         );
+    }
+
+    public function clearArray(array $result): array
+    {
+        $sort = [];
+        foreach ($result as $item => $value) {
+            $sort[$item] = [
+                'id' => 'https://vk.com/id' . $value['id'],
+                'first_name' => $value['first_name'],
+                'last_name' => $value['last_name'],
+                'mobile_phone' => $value['mobile_phone']
+            ];
+        }
+        return $sort;
     }
 
 
